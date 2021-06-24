050670805f648ddf5dbb582d744c6905a26a5eee
Mike Bombay <82775499+MikeBombay@users.noreply.github.com>
2021-06-23 21:50:51 -0400

    more layout

--- script.js
+++ script.js
@@ -2,3 +2,8 @@ moment(Date);
 $("#currentDay").text(moment().format('dddd MMMM Do YYYY, h:mm a'));
 
 var currentTime = moment();
+
+var currentHour = moment().hour();
+console.log(currentHour);
+
+
