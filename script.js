bec41d18e6b1b1324a6d10d61450df920332a984
Mike Bombay <82775499+MikeBombay@users.noreply.github.com>
2021-06-26 14:23:51 -0400

    save and render

--- script.js
+++ script.js
@@ -4,6 +4,54 @@ $("#currentDay").text(moment().format('dddd MMMM Do YYYY, h:mm a'));
 var currentTime = moment();
 
 var currentHour = moment().hour();
-console.log(currentHour);
+//console.log(currentHour);
+
+function checkHour() {
+$(".time-block").each(function () {
+    var thisHour = parseInt($(this).attr("id"));
+    console.log(thisHour)
+
+    if (thisHour < currentHour) {
+        $(this).addClass("past");
+        $(this).removeClass("future");
+        $(this).removeClass("present");
+    }
+    else if (thisHour === currentHour) {
+        $(this).removeClass("past");
+        $(this).addClass("present");
+        $(this).removeClass("future");
+    }
+    else {
+        $(this).removeClass("present");
+        $(this).removeClass("past");
+        $(this).addClass("future");
+    }})};
+    
+
+  function renderSaved(){
+    $(".plan").each(function(){
+        var saved = $(this).attr("id");
+        $(this).val(localStorage.getItem(saved));
+    });
+};
+ 
+
+    $(".saveBtn").click(function (event) {
+        event.preventDefault();
+     
+        var savedPlan = $(this).siblings(".plan").val();
+        var timeOf = $(this).parent().attr('id');
+        
+
+      console.log(savedPlan);
+      console.log(timeOf);
+
+      
+
+        localStorage.setItem(timeOf, savedPlan);
 
 
+    });
+
+    renderSaved();
+    checkHour();
